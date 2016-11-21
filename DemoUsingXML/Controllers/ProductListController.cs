755ea18407d80e9c8dc3c50a6239863cac54e6b7
perodinamit <matej.curcic@gmail.com>
2016-11-21 15:08:03 +0100

    Read from XML file to page into table

---
+++ DemoUsingXML/Controllers/ProductListController.cs
@@ -0,0 +1,17 @@
+﻿using Demo.Helper;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace DemoUsingXML.Controllers
+{
+    public class ProductListController : Controller
+    {
+        // GET: ProductList  
+        public ActionResult Index()
+        {
+            XMLReader readXML = new XMLReader();
+            var data = readXML.RetrunListOfProducts();
+            return View(data.ToList());
+        }
+    }
+}
